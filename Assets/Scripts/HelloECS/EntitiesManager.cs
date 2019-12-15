f3a7f8bbe13d571766f644620114007f3b233cdc
Mr.Liao <312998164@qq.com>
2019-12-16 00:30:32 +0800

    DOTS learn

---
+++ Assets/Scripts/HelloECS/EntitiesManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class EntitiesManager : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public float RotationSpeed=10f;
+     //将gameObject转换为实体所必须的接口
+    //在gameObject上还需要挂载自带的GameObject Convert to Entity脚本
+    void IConvertGameObjectToEntity.Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        //创建一个RotationComponent
+        RotationComponent rotationComponent = new RotationComponent{rotationSpeed=this.RotationSpeed};
+        //将这个RotationComponent到当前脚本挂载的gameObject所转换类型的实体上，然后由EntityManager统一管理
+        dstManager.AddComponentData(entity, rotationComponent);
+    }
+}
---
+++ Assets/Scripts/HelloECS/RotationSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+using Unity.Transforms;
+
+public class RotationSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        //凡是挂载了RotationComponent的Entity都会被这个函数所改变旋转角度
+        Entities.ForEach((ref RotationComponent rotationComponent,
+        ref Rotation rotation)=>
+        {
+            //在每帧计算旋转角度
+            rotation.Value=math.mul(math.normalize(rotation.Value), //将原旋转角度标准化后，再乘以通过轴旋转后得到的旋转角度，得到在一帧时间后物体的旋转角度
+            quaternion.AxisAngle(math.up(),rotationComponent.rotationSpeed*Time.DeltaTime));
+        });
+    }
+}
